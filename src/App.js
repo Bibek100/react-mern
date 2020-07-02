5f1ca1a3bbb5264aa814d3c46ae9b62aec08a060
vivek <vivek.dahal10@gmail.com>
2020-07-02 13:47:28 -0500

    New places added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
 const App = () => {
-  return <h1>A react App</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
