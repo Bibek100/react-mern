903236fac3272821ed53ce359280677a92687c54
vivek <vivek.dahal10@gmail.com>
2020-07-02 14:16:28 -0500

    Props added for useritems to display userlist

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,3 +1,30 @@
-import react from "react";
+import React from "react";
+import "./UserList.css";
+import UserItem from "./UserItem";
 
-const UserList = (props) => {};
+const UserList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found</h2>
+      </div>
+    );
+  }
+  return (
+    <ul>
+      {props.items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default UserList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,17 @@
 import React from "react";
+
+import UserList from "../components/UserList";
 const Users = () => {
-  return <h2>User Works</h2>;
+  const USERS = [
+    {
+      id: "1",
+      name: "Bibek",
+      image:
+        "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/83569715_1432766646901525_7817053521982783488_n.jpg?_nc_cat=105&_nc_sid=d4cf07&_nc_ohc=zPRshzLkYYwAX-fwQYJ&_nc_ht=scontent-dfw5-1.xx&oh=ccf4d0c884ee49c7b89dd2607841cfaf&oe=5F23F9CC",
+      places: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
