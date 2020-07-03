f5a2903931754bfdabafc115657c816a3c6fa6af
vivek <vivek.dahal10@gmail.com>
2020-07-03 16:07:22 -0500

    Animation addded to drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -5,21 +5,27 @@ import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavsLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerOpen] = useState(false);
   const openDrawer = () => {
     setDrawerOpen(true);
   };
+
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  };
   return (
     <React.Fragment>
-      {drawerIsOpen && (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
-      )}
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
       <MainHeader>
         <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
