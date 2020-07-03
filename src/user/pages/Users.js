28bd22f0954e248bfd879e23351f6ab3d9007f88
vivek <vivek.dahal10@gmail.com>
2020-07-03 00:20:40 -0500

    Navigation page added with drawer and main header

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
   const USERS = [
     {
       id: "1",
-      name: "Bibek",
+      name: "Bibek Dahal",
       image:
         "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/83569715_1432766646901525_7817053521982783488_n.jpg?_nc_cat=105&_nc_sid=d4cf07&_nc_ohc=zPRshzLkYYwAX-fwQYJ&_nc_ht=scontent-dfw5-1.xx&oh=ccf4d0c884ee49c7b89dd2607841cfaf&oe=5F23F9CC",
       places: 3,
