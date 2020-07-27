cce6b1fda312e70a928b74b8d9fa7cd92c38b490
Bibek100 <vivek.dahal10@gmail.com>
2020-07-26 23:27:38 -0500

    Form wide state maintained using usereducer

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,61 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from "../../shared/utils/validators";
 import "./NewPlace.css";
+
+const formReducer = (state, action) => {
+  switch ((action, type)) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const input in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        input: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+          isValid: formIsValid,
+        },
+      };
+    default:
+      return state;
+  }
+};
 const NewPlace = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+  const inputHandler = useCallback(
+    (id, value, isValid) => {
+      dispatch({
+        type: "INPUT_CHANGE",
+        value: value,
+        isValid: isValid,
+        inputId: id,
+      });
+    },
+    [dispatch]
+  );
+
   return (
     <form className="place-form">
       <Input
@@ -17,7 +65,7 @@ const NewPlace = () => {
         label="title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="please ener valid title"
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         id="description"
@@ -25,8 +73,11 @@ const NewPlace = () => {
         label="title"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="please ener valid descrption"
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
+      <Button type="submit" disabled={!formState.isValid}>
+        Add Place
+      </Button>
     </form>
   );
 };
